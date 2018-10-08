468fcabce6e4b3a3c225abe34be2699f24be3d5f
chenliw <chenliw@yonyou.com>
2018-10-08 16:16:07 +0800

    服务

---
+++ src/main/java/com/pku/controller/StoreController.java
@@ -0,0 +1,30 @@
+package com.pku.controller;
+
+import com.pku.domain.CarType;
+import com.pku.domain.RespCode;
+import com.pku.domain.RespEntity;
+import com.pku.domain.StoreInfo;
+import com.pku.service.CarService;
+import com.pku.service.StoreService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin
+@RestController
+
+@RequestMapping({"/store"})
+public class StoreController {
+    @Autowired
+    StoreService storeService ;
+
+    @RequestMapping(value="/getStoreList",method = RequestMethod.GET)
+    @ResponseBody
+    public RespEntity getStoreList(){
+        List<StoreInfo> list ;
+        list =  storeService.queryStoreInfo();
+        return  new RespEntity(RespCode.SUCCESS, list);
+    }
+
+}
---
+++ src/main/java/com/pku/service/StoreService.java
@@ -0,0 +1,19 @@
+package com.pku.service;
+
+import com.pku.domain.CarType;
+import com.pku.domain.StoreInfo;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Mapper
+@Service
+public interface StoreService {
+
+    @Select("select * from storeinfo")
+    List<StoreInfo> queryStoreInfo();
+
+}
